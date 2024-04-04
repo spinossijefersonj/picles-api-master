fd64096945932c8decc2563393054b83ccfa9a70
Spinossi <spinossi_jeferson@outlook.com>
2024-04-03 22:09:52 -0300

    Primeiro Commit

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Length,} from "class-validator"
+
 export default class UpdateShelterDetailsUseCaseInput {
+	@IsString()
+	@IsNotEmpty()
 	name: string
+	@IsNotEmpty()
+	@IsString()
+	@Length(10,11)
 	whatsapp: string
+	@IsNotEmpty()
+	@IsEmail()
+	@IsString()
 	email: string
+	@Length(10,11)
+	@IsNotEmpty()
+	@IsString()
+	@IsNumber()
 	phone: string
 
 }
