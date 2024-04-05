79c03539a031ce284ad93b21b8194885f5315c37
Spinossi <spinossi_jeferson@outlook.com>
2024-04-04 23:38:35 -0300

    Interação com banco de dados

--- src/shelter/shelter.module.ts
+++ src/shelter/shelter.module.ts
@@ -2,15 +2,22 @@ import { Module } from '@nestjs/common';
 import { ShelterController } from './shelter.controller';
 import ShelterTokens from './shelter.tokens';
 import GetShelterDetailsUseCase from './usecases/get.shelter.details.usecase';
+import GetShelterDetailsUseCaseOutput from './usecases/dtos/get.shelter.details.usecase.output';
 
 import { MongooseModule } from '@nestjs/mongoose';
-import { Shelter } from './schemas/shelter.schemas';
+import { Shelter, ShelterSchima } from '@nestjs/mongoose';
 
 @Module({
   controllers: [ShelterController],
-  providers: [{
+
+  imports: [
+    MongooseModule.forFeature([{name: Shelter.name, schema: ShelterSchima}])
+  ],
+  providers: [
+  {
 	provide: ShelterTokens.getShelderDetailsUseCase,
 	useClass: GetShelterDetailsUseCase
-  }]
+  }
+]
 })
 export class ShelterModule {}
